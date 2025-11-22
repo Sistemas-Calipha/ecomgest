44c9abb6871574df7537282480c1d195b546c00f
Erick <sistemas.caliphadigital@gmail.com>
2025-11-22 12:54:16 -0300

    Contenido

--- ecomgest-frontend/src/pages/Inventario/Productos.jsx
+++ ecomgest-frontend/src/pages/Inventario/Productos.jsx
@@ -1,7 +1,7 @@
 export default function InventarioProductos() {
   return (
     <div className="p-6">
-      <h1 className="text-2xl font-bold mb-4">Ventas</h1>
+      <h1 className="text-2xl font-bold mb-4">Inventario</h1>
       <p className="text-gray-600 dark:text-gray-300 text-sm">
         Contenido del módulo próximamente...
       </p>
