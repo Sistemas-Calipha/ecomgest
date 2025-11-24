2c5944fb74042f895ea0790ca3cb31f2279c5cf7
Erick <sistemas.caliphadigital@gmail.com>
2025-11-24 11:34:31 -0300

    Conigurando

--- ecomgest-backend/src/routes/dashboard.routes.js
+++ ecomgest-backend/src/routes/dashboard.routes.js
@@ -1,11 +1,11 @@
 // src/routes/dashboard.routes.js
 import { Router } from "express";
 import { getMyDashboardConfig, saveDashboardConfig } from "../controllers/dashboard.controller.js";
-import { authMiddleware } from "../middlewares/auth.middleware.js";
+import { verifyToken } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.get("/my", authMiddleware, getMyDashboardConfig);
-router.post("/save", authMiddleware, saveDashboardConfig);
+router.get("/my", verifyToken, getMyDashboardConfig);
+router.post("/save", verifyToken, saveDashboardConfig);
 
 export default router;
