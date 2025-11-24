b7ea92a918352e3b5daacf3165956f3562537eee
Erick <sistemas.caliphadigital@gmail.com>
2025-11-24 13:31:12 -0300

    Configurando....

--- ecomgest-backend/src/server.js
+++ ecomgest-backend/src/server.js
@@ -1,7 +1,6 @@
 // src/server.js
 
 import app from "./app.js";
-import "./config/dotenv.js";       // Carga variables de entorno
 import "./config/supabase.js";    // Conecta Supabase
 
 const PORT = process.env.PORT || 3000;
